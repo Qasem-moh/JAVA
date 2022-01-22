ee235bb4c3c4356ad655f6b9a7ee2b8511ff2e70
qasem-moh <2171246jo@s.aou.edu.jo>
2022-01-22 23:31:35 +0200

    java

---
+++ java/mavenproject1/src/main/java/com/mycompany/mavenproject1/whileloop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author qasem
+ */
+public class whileloop {
+
+    public static void main(String args[]) {
+        int d = 5;
+        while (d < 8) {
+            System.out.println("Hi");
+            d++;
+        }
+    }
+}
