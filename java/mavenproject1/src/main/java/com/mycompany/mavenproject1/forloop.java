cef9436a2612a5f35eeb1a3f614152681dd8e967
qasem-moh <2171246jo@s.aou.edu.jo>
2022-01-23 00:50:35 +0200

    java

---
+++ java/mavenproject1/src/main/java/com/mycompany/mavenproject1/forloop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author qasem
+ */
+public class forloop {
+
+    public static void main(String args[]) {
+        int c = 0;
+        for (; c < 6;) {
+            System.out.println("res" + c);
+            c++;
+        }
+    }
+}
