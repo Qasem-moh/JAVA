1a5d4608be53a7ae2b527970142a17572ad7381a
qasem-moh <2171246jo@s.aou.edu.jo>
2022-01-27 18:38:56 +0200

    JAVA

---
+++ java/mavenproject1/src/main/java/com/mycompany/mavenproject1/javaFunction.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author qasem
+ */
+public class javaFunction {
+
+    static void fun() {
+        System.out.println("Qasem");
+        System.out.println("Qasem");
+        System.out.println("Qasem");
+        System.out.println("Qasem");
+        System.out.println("Qasem");
+
+    }
+
+    static int sumNumber(int n1, int n2) {
+        int sum = n1 + n2;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        fun();
+        
+        System.out.println(sumNumber(5,5));
+    }
+}
--- java/mavenproject1/src/main/java/com/mycompany/mavenproject1/labled_break_and_continue.java
+++ java/mavenproject1/src/main/java/com/mycompany/mavenproject1/labled_break_and_continue.java
@@ -5,9 +5,24 @@
 package com.mycompany.mavenproject1;
 
 /**
+ * labeled break statement labeled continue statement
  *
  * @author qasem
  */
 public class labled_break_and_continue {
-    
+
+    public static void main(String args[]) {
+        stop:
+        {
+            for (int row = 1; row <= 10; row++) {
+                for (int j = 1; j <= 10; j++) {
+                    if (row == 5) {
+                        break stop;
+                    }
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+        }
+    }
 }
